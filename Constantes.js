a089c625eeae0b2f184ed475fe188b802e1317c2
melendil <melendil@melendil-laptop>
2011-10-02 23:14:47 +0200

    regroupemet constantes

--- Constantes.js
+++ Constantes.js
@@ -1,5 +1,14 @@
 var JSFOURMIS = JSFOURMIS || {};
 (function() {
+	
+	JSFOURMIS.Directions = {
+			NORD : 1,
+			SUD : -1,
+			EST : 2,
+			OUEST : -2,
+			AUCUNE : 3
+		};
+	
 	JSFOURMIS.AnglesRotation = {
 		DROITE : 1,
 		GAUCHE : 2,
